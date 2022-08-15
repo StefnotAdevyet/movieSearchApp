8b8baa1fdacfd31900005e7a99445e0e561553a6
developer <steph.jackson556@gmail.com>
2022-08-15 17:33:35 -0400

    restructuring to MVVM architecture

---
+++ MovieSearch/MovieSearch/Views/SearchViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  MovieSearch
+//
+//  Created by developer on 8/15/22.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBAction func searchBtn(_ sender: UIButton) {
+    }
+    @IBOutlet weak var searchTable: UITableView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
+extension SearchViewController {
+    
+}
+
