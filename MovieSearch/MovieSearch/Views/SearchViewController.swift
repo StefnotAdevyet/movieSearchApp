c0d5129fcb2fec84458c5fdd5a0f59b1e0df03e7
developer <steph.jackson556@gmail.com>
2022-08-15 17:56:06 -0400

    connected views to ViewController

--- MovieSearch/MovieSearch/Views/SearchViewController.swift
+++ MovieSearch/MovieSearch/Views/SearchViewController.swift
@@ -16,13 +16,23 @@ class SearchViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        searchTable.delegate = self
+        searchTable.dataSource = self
         // Do any additional setup after loading the view.
     }
 
 
 }
 
-extension SearchViewController {
+extension SearchViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        UITableViewCell()
+    }
+    
     
 }
 
